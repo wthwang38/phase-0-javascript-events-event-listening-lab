0dfbe34a4b7e42eaf94d3afee2d8e1e44cca8387
William Hwang <willhwang90@gmail.com>
2022-05-03 10:29:35 -0400

    commit tree 2b8592dd0bd600f0f70aedf873318c82cd8ed9c1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    input.addEventListener('click', alertMe);
+
 }
+
+
+const input = document.querySelector('input');
+console.log(input);
+
+
+function alertMe(e) {
+    alert("I was CLICKED!");
+    console.log(e)
+}
+
+addingEventListener();
